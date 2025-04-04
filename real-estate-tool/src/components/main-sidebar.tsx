6d855142e576439662f48e61ec9b41d3b2373f4b
jitt <jhlim.2023@smu.edu.sg>
2025-04-05 00:10:18 +0800

    fix logo

--- real-estate-tool/src/components/main-sidebar.tsx
+++ real-estate-tool/src/components/main-sidebar.tsx
@@ -8,6 +8,7 @@ import {
   LogOut,
   Settings,
   Users,
+  Image
 } from "lucide-react";
 
 export function Sidebar() {
@@ -16,7 +17,8 @@ export function Sidebar() {
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/" className="flex items-center gap-2 font-semibold">
-            <Building className="h-6 w-6" />
+            {/* <Building className="h-6 w-6" /> */}
+            <img className="g-6 w-6" src="image.png"></img>
             <span>ERA Agent Hub</span>
           </Link>
         </div>
